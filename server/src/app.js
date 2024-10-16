b522d2efbe18c0f501ebc33edec7009beb9b1068
Aasif khan <akay41024@gmail.com>
2024-10-17 05:05:55 +0530

    Update cors dependency and configure app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}))
 
 app.use(cookieParser());
 
@@ -17,7 +22,7 @@ app.use('/api/v1/users', addUser);
 
 
 app.get('/', (req, res) => {
-    res.send('Hello from server side!!!!!');
+    res.send('Hello from server');
 })
 
 export { app };
