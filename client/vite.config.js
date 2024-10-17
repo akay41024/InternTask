f597d06e38e40335c76602dcf268375083def2a6
Aasif khan <akay41024@gmail.com>
2024-10-17 06:15:59 +0530

    Update proxy URL in vite.config.js and fix server dev script

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://intern-task-deso.vercel.app/',
+      '/api': 'http://localhost:3000',
     },
   },
 })
