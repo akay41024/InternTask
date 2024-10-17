c3d2553b23b3ce38864bfbae653d680558fe5c27
Aasif khan <akay41024@gmail.com>
2024-10-18 00:27:04 +0530

    Refactor API proxy configuration in Vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => {
           target: env.VITE_API_URL,
           changeOrigin: true,
           secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
         },
       },
     },
